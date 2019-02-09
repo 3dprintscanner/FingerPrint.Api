98971f7ca0dc5a6e0b964ec01dfe54c7f30439ff
3dprintscanner <1988anthonythomas@gmail.com>
2019-02-09 14:21:22 +0000

    First commit

---
+++ FingerPrint.Api/Controllers/FingerPrintController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fingerprint.Impl;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SourceAFIS.Extraction.Minutiae;
+using SourceAFIS.Simple;
+
+namespace FingerPrint.Api.Controllers
+{
+    public class FingerPrintController : Controller
+    {
+
+        [HttpPost("fingerprint")]
+        public ActionResult<string> Post(IFormFile file)
+        {
+
+            var impl = new FingerPrintImpl();
+
+
+            var content = file.OpenReadStream();
+
+            var signature  = impl.DoFingerPrint(content);
+
+            return signature;
+        }
+    }
+}
---
+++ Fingerprint.Impl/FingerprintImpl.cs
@@ -0,0 +1,29 @@
+﻿using SourceAFIS.Simple;
+using System;
+using System.IO;
+
+namespace Fingerprint.Impl
+{
+    public class FingerPrintImpl
+    {
+
+        public string DoFingerPrint(Stream stream)
+        {
+
+            AfisEngine Afis = new AfisEngine();
+
+            var fs = new SourceAFIS.Simple.Fingerprint();
+
+            fs.AsBitmap = new System.Drawing.Bitmap(stream);
+
+
+            var person = new Person();
+            person.Fingerprints.Add(fs);
+
+            Afis.Extract(person);
+
+            return person.Fingerprints[0].AsXmlTemplate.ToString();
+
+        }
+    }
+}
